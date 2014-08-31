f65502ec8cdcc36494be4d349cdce87ea3a561d3
Wilker <Wilker@PC>
2014-08-31 20:46:47 -0300

    Exercício 12 Concluído

---
+++ Exercicio12.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   Exercicio12.h
+ * Author: Wilker
+ *
+ * Created on 24 de Agosto de 2014, 19:24
+ */
+
+#ifndef EXERCICIO12_H
+#define	EXERCICIO12_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+char situacao(float p1, float p2, float p3, int faltas, int aulas, float *media);
+float med(float p1, float p2, float p3);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* EXERCICIO12_H */
+
