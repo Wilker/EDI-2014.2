cb60cafb10fe8a4524640d79ece49f3249a5b9b0
Wilker <Wilker@PC>
2014-11-16 12:23:15 -0200

    Exercício 1 concluído!

---
+++ Lista_3/main.c
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.c
+ * Author: Wilker
+ *
+ * Created on 15 de Novembro de 2014, 23:20
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "AB.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Exercício 1
+    Arv* raiz = arv_cria(2,
+                         arv_cria(7,
+                                   arv_cria(2,NULL,NULL),
+                                   arv_cria(6,
+                                             arv_cria(5,NULL,NULL),
+                                             arv_cria(11,NULL,NULL))),
+                         arv_cria(5,
+                                   NULL,
+                                   arv_cria(9,
+                                             arv_cria(4,NULL,NULL),
+                                             NULL)));
+    arv_imprime(raiz);
+    return (EXIT_SUCCESS);
+}
+
