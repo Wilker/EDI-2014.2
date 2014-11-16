68f384106d6ba574f7f395574e57d1937d834cca
Wilker <Wilker@PC>
2014-11-16 12:21:52 -0200

    Struct Arvore AB implementada!

---
+++ Lista_3/AB.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   AB.h
+ * Author: Wilker
+ *
+ * Created on 15 de Novembro de 2014, 23:22
+ */
+
+#ifndef AB_H
+#define	AB_H
+
+typedef struct Arv {
+    int info;
+    struct Arv* esq;
+    struct Arv* dir;
+}Arv;
+
+
+
+Arv* arv_cria(int x, Arv* sae, Arv* sad);
+void arv_libera (Arv* a);
+int arv_contem (Arv* a, int x);
+void arv_imprime (Arv* a);
+#endif	/* AB_H */
+
